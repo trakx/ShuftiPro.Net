19544814c4a3fe3e45578efbad2a679fc85b4334
RafaelMeloTrakx <rafael@trakx.io>
2021-10-04 18:16:37 -0300

    Added new parameters in 'verification_instructions' element

--- src/ShuftiPro/Contracts/Abstractions/IShuftiProVerificationInstruction.cs
+++ src/ShuftiPro/Contracts/Abstractions/IShuftiProVerificationInstruction.cs
@@ -11,10 +11,24 @@ namespace ShuftiPro.Contracts.Abstractions
 
         [JsonConverter(typeof(ShuftiProBoolToIntConverter))]
         [JsonProperty("allow_photocopy", NullValueHandling = NullValueHandling.Ignore)]
+
         bool? AllowPhotocopy { get; set; }
 
         [JsonConverter(typeof(ShuftiProBoolToIntConverter))]
         [JsonProperty("allow_laminated", NullValueHandling = NullValueHandling.Ignore)]
         bool? AllowLaminated { get; set; }
+
+        [JsonConverter(typeof(ShuftiProBoolToIntConverter))]
+        [JsonProperty("allow_screenshot", NullValueHandling = NullValueHandling.Ignore)]
+        bool? AllowScreenshot { get; set; }
+
+        [JsonConverter(typeof(ShuftiProBoolToIntConverter))]
+        [JsonProperty("allow_cropped", NullValueHandling = NullValueHandling.Ignore)]
+        bool? AllowCropped { get; set; }
+
+        [JsonConverter(typeof(ShuftiProBoolToIntConverter))]
+        [JsonProperty("allow_scanned", NullValueHandling = NullValueHandling.Ignore)]
+        bool? AllowScanned { get; set; }
+
     }
 }
